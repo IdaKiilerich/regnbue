efc2d5e2faa7af8d58a83ce33894579f538dcc56
IdaKiilerich <94226083+IdaKiilerich@users.noreply.github.com>
2021-11-13 16:57:00 +0100

    bug fixes

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
+input.onGesture(Gesture.Shake, function () {
+    anim_random(10)
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    basic.showString("M'Lady")
+    anim_rainbow(1)
+})
 input.onButtonPressed(Button.A, function () {
-    anim_rainbow(10)
+    anim_rainbow(25)
 })
 function hart_beat_step () {
     if (hart_state) {
@@ -12,9 +19,13 @@ function hart_beat_step () {
 }
 function anim_night_step () {
     if (animating == 0) {
+        strip.setBrightness(100)
         strip_long_range.setPixelColor(night_pos_1, neopixel.colors(NeoPixelColors.Black))
         strip_long_range.setPixelColor(night_pos_2, neopixel.colors(NeoPixelColors.Black))
         if (night_pos_1 > 19) {
+            for (let indeks = 0; indeks <= 9; indeks++) {
+                strip_short_range.setPixelColor(indeks, neopixel.rgb(randint(50, 255), randint(50, 255), randint(50, 255)))
+            }
             night_pos_1 = 0
         } else {
             night_pos_1 += 1
@@ -43,13 +54,10 @@ function anim_rainbow (loops2: number) {
     strip.clear()
     animating = 0
 }
-input.onGesture(Gesture.ScreenDown, function () {
-    basic.showString("M'Lady")
-})
 function init_strip () {
     strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
-    strip_short_range = strip.range(20, 10)
-    strip_long_range = strip.range(0, 20)
+    strip_short_range = strip.range(0, 10)
+    strip_long_range = strip.range(10, 20)
     strip.setBrightness(128)
     basic.showIcon(IconNames.Skull)
     hart_state = 0
@@ -58,10 +66,10 @@ function init_strip () {
     night_pos_2 = 0
 }
 input.onButtonPressed(Button.AB, function () {
-    anim_random(100)
+    anim_random(25)
 })
 input.onButtonPressed(Button.B, function () {
-    anim_hunt(10)
+    anim_hunt(25)
 })
 function anim_hunt (loops2: number) {
     animating = 1
@@ -69,7 +77,7 @@ function anim_hunt (loops2: number) {
     strip.setPixelColor(randint(0, 29), neopixel.colors(NeoPixelColors.Blue))
     strip.setPixelColor(randint(0, 29), neopixel.colors(NeoPixelColors.Red))
     for (let index = 0; index < loops2; index++) {
-        for (let indeks = 0; indeks <= 29; indeks++) {
+        for (let indeks2 = 0; indeks2 <= 29; indeks2++) {
             strip.rotate(1)
             strip.show()
             basic.pause(100)
@@ -83,34 +91,23 @@ function anim_random (loops2: number) {
     strip.clear()
     for (let index = 0; index < loops2; index++) {
         for (let index = 0; index < 30; index++) {
-            strip.setPixelColor(randint(0, 29), neopixel.rgb(randint(0, 255), randint(0, 255), randint(0, 255)))
+            strip.setPixelColor(randint(0, 29), neopixel.rgb(randint(25, 255), randint(25, 255), randint(25, 255)))
         }
+        strip.show()
         basic.pause(100)
     }
     strip.clear()
     animating = 0
 }
-control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_DOWN, function () {
-    animating = 8
-    strip_short_range.showRainbow(1, 360)
-    strip_long_range.showRainbow(1, 360)
-})
-let hart_divider = 0
 let strip_short_range: neopixel.Strip = null
-let strip: neopixel.Strip = null
 let night_pos_2 = 0
 let night_pos_1 = 0
 let strip_long_range: neopixel.Strip = null
+let strip: neopixel.Strip = null
 let animating = 0
 let hart_state = 0
 init_strip()
 basic.forever(function () {
-    if (hart_divider == 4) {
-        hart_beat_step()
-        hart_divider = 0
-    } else {
-        hart_divider += 1
-    }
     anim_night_step()
     basic.pause(25)
 })
